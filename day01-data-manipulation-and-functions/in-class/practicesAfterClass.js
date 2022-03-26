56ff5ecf388789f0ec9f499419a2d1a3a0bc037e
KaiquanMah <7814298+KaiquanMah@users.noreply.github.com>
2022-03-26 15:10:43 +0800

    Initial commit

---
+++ day01-data-manipulation-and-functions/in-class/practicesAfterClass.js
@@ -0,0 +1,36 @@
+// Math Operations
+// + - * / %
+
+// Variables
+var a = 123;
+var b = "def";
+var c = "234";
+var d = a + Number(c);
+
+// check data type
+typeof 5;
+typeof "some string";
+
+// Functions
+var functionAName = function (input) {
+  //perform some operations
+
+  return; //return someOutput;
+};
+
+var output = functionAName(input);
+
+var sayHello = function () {
+  return "Hello";
+};
+sayHello();
+// 'Hello'
+
+// template literals
+// enclose string in backticks ``
+// enclose variable in ${variableName}
+// var myOutputValue = `Hi! ${input} is equal to ${distanceInMiles} miles.`
+
+// Naming convention
+// variable - celsius
+// function - convertFahrenheitToCelsius()
