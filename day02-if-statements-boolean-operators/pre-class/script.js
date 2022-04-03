716e7a2824f19dea9f2b3942a01d541cef297922
KaiquanMah <7814298+KaiquanMah@users.noreply.github.com>
2022-04-03 11:49:37 +0800

    initial commit

--- day02-if-statements-boolean-operators/pre-class/script.js
+++ day02-if-statements-boolean-operators/pre-class/script.js
@@ -1,35 +1,89 @@
-var orangeJuiceMain = function (input) {
+// WITH ASSUMPTION
+var orangeJuiceMain = function (nExpectedGuests) {
   // Attempt the Orange Juice Calculator exercise from the Functions II module below with orangeJuiceMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  // 4 oranges 1 glass of juice
+  // 1 guest drinks 2 glasses of juice on avg
+  var orangePerGlass = 4;
+  var glassPerGuest = 2;
+  var totalOranges = orangePerGlass * glassPerGuest * nExpectedGuests;
+  return totalOranges;
+};
+// input 2 => output 16
+
+// WITHOUT ASSUMPTION
+var orangeJuiceMain = function (nExpectedGuests, glassPerGuest = 2) {
+  // Attempt the Orange Juice Calculator exercise from the Functions II module below with orangeJuiceMain as the main function.
+  // 4 oranges 1 glass of juice
+  // 1 guest drinks 2 glasses of juice on avg
+  var orangePerGlass = 4;
+  // var glassPerGuest = 2;
+  var totalOranges = orangePerGlass * glassPerGuest * nExpectedGuests;
+  return totalOranges;
 };
 
-var paintMain = function (input) {
+// WITH ASSUMPTION
+var paintMain = function (costPerLitrePaint) {
   // Attempt the House Paint exercise from the Functions II module below with paintMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var areaPerRoom = 3 * 3;
+  var nRooms = 6;
+  var areaPerLitre = 300; //square metres per litre
+  var nCoatsPaint = 2;
+  var totalPaint = (areaPerRoom * nRooms * nCoatsPaint) / areaPerLitre;
+  var totalPaintCost = totalPaint * costPerLitrePaint;
+  return totalPaintCost;
 };
 
-var randomDiceMain = function (input) {
+// WITHOUT ASSUMPTION
+var paintMain = function (costPerLitrePaint, nRooms, nCoatsPaint) {
+  // Attempt the House Paint exercise from the Functions II module below with paintMain as the main function.
+  var areaPerRoom = 3 * 3;
+  // var nRooms = 6;
+  var areaPerLitre = 300; //square metres per litre
+  // var nCoatsPaint = 2;
+  var totalPaint = (areaPerRoom * nRooms * nCoatsPaint) / areaPerLitre;
+  var totalPaintCost = totalPaint * costPerLitrePaint;
+  return totalPaintCost;
+};
+
+var rollDice = function () {
+  // Generate a decimal from 0 through 6, inclusive of 0 and exclusive of 6.
+  var randomDecimal = Math.random() * 6;
+
+  // Remove the decimal with the floor operation.
+  // This will be an integer from 0 to 5 inclusive.
+  var randomInteger = Math.floor(randomDecimal);
+
+  // Add 1 to get valid dice rolls of 1 through 6 inclusive.
+  var diceNumber = randomInteger + 1;
+
+  return diceNumber;
+};
+
+var randomDiceMain = function () {
   // Attempt the Random Dice Rolls exercise from the Intro to Logic and Control Flow module below with randomDiceMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  // Math.random() => 0 up to BUT NOT INCLUDING 1; drawn at random
+  var randomFloatBeforeSix = Math.random() * 6;
+  var randomIntBeforeSix = Math.floor(randomFloatBeforeSix);
+  // because a normal dice wont have 0; and would have 6
+  // we have to increment the integers by 1
+  var randomIntDiceRoll = randomIntBeforeSix + 1;
+  return randomIntDiceRoll;
 };
 
 var secretPhraseMain = function (input) {
   // Attempt the Secret Phrase exercise from the If, Else, Else If module below with secretPhraseMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var diceGameMain = function (input) {
   // Attempt the Dice Game exercise from the If, Else, Else If module below with diceGameMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var twiceGuessMain = function (input) {
   // Attempt the Twice the Guess exercise from the If, Else, Else If module below with twiceGuessMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
--- day03-program-state/in-class/script.js
+++ day03-program-state/in-class/script.js
@@ -1,41 +1,72 @@
-var secretWordBaseMain = function (input) {
+var RandomNumber = function () {
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomDecimal);
+
+  var number = randomInteger + 1;
+  return number;
+};
+
+var computerWord = "";
+var countCorrectGuess = 0;
+var countCorrectGuessLeftToWin = 0;
+
+var secretWordBaseMain = function (userInput) {
   // Complete the Base: Secret Word exercise below with secretWordBaseMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var computerNumber = RandomNumber();
+
+  if (computerNumber === 1) {
+    computerWord = "banana";
+  } else if (computerNumber === 2) {
+    computerWord = "chisel";
+  } else if (computerNumber === 3) {
+    computerWord = "faucet";
+  } else {
+    computerWord = "error";
+  }
+
+  var correctGuess = userInput == computerWord;
+  countCorrectGuess = countCorrectGuess + correctGuess;
+  countCorrectGuessLeftToWin = 2 - countCorrectGuess;
+
+  if (countCorrectGuess < 2) {
+    return `User guessed ${userInput} and computer guessed ${computerWord}. The guess was ${correctGuess}. Number of correct guesses so far is ${countCorrectGuess}. Number of correct guesses needed to win is ${countCorrectGuessLeftToWin}`;
+  } else if (countCorrectGuess === 2) {
+    return `User guessed ${userInput} and computer guessed ${computerWord}. The guess was ${correctGuess}. Number of correct guesses so far is ${countCorrectGuess}. Number of correct guesses left to win is ${countCorrectGuessLeftToWin}. You won!`;
+  }
 };
 
 var secretWordTwiceRowMain = function (input) {
   // Complete the Comfortable: Secret Word Twice in a Row exercise below with secretWordTwiceRowMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var secretWordXRowMain = function (input) {
   // Complete the Comfortable: Secret Word X in a Row exercise below with secretWordXRowMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var diceWithinMain = function (input) {
   // Complete the More Comfortable: Dice Within exercise below with diceWithinMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var diceWithin2DiceMain = function (input) {
   // Complete the More Comfortable: Dice Within with 2 Dice exercise below with diceWithin2DiceMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var dice4DMain = function (input) {
   // Complete the More Comfortable: Dice 4D exercise below with dice4DeMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var secretWordTwice2Main = function (input) {
   // Complete the More Comfortable: Secret Word Twice in a Row 2 exercise below with secretWordTwice2Main as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
